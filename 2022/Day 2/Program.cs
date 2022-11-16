86b9ad13ccc82f019953571d872bdf05dbdcfc75
Henrik Kurelid <henrik@kurelid.se>
2022-11-16 15:43:44 +0100

    Prepared projects for AoC 2022.

---
+++ 2022/Day 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
