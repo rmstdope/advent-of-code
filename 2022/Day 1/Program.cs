e72bf59006b76c0734125ae0f41d2b83962d01a3
Henrik Kurelid <henrik@kurelid.se>
2022-12-01 06:11:00 +0100

    First problem of 2022

--- 2022/Day 1/Program.cs
+++ 2022/Day 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -6,8 +7,25 @@ namespace AdventOfCode
     {
         static void Main()
         {
+            List<int> calories = new();
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == "")
+                {
+                    calories.Add(sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(lines[i]);
+                }
+            }
+            calories.Add(sum);
+            calories.Sort();
+            Console.WriteLine(calories[calories.Count - 1]);
+            Console.WriteLine(calories[calories.Count - 1] + calories[calories.Count - 2] + calories[calories.Count - 3]);
         }
     }
 }
