41ccc1aec8b13eca431146c287a17fc4b67a631b
Henrik Kurelid <henrik@kurelid.se>
2022-12-02 06:17:18 +0100

    Completed Day 2.

--- 2022/Day 2/Program.cs
+++ 2022/Day 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AdventOfCode
 {
@@ -7,7 +8,90 @@ namespace AdventOfCode
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                int p = GetPoints(line[0], line[2]);
+                sum += p;
+
+            }
+            Console.WriteLine(sum);
+
+            sum = 0;
+            foreach (string line in lines)
+            {
+                char x = 'a';
+                if (line[2] == 'X')
+                {
+                    if (line[0] == 'A')
+                        x = 'Z';
+                    if (line[0] == 'B')
+                        x = 'X';
+                    if (line[0] == 'C')
+                        x = 'Y';
+                }
+                if (line[2] == 'Y')
+                {
+                    if (line[0] == 'A')
+                        x = 'X';
+                    if (line[0] == 'B')
+                        x = 'Y';
+                    if (line[0] == 'C')
+                        x = 'Z';
+                }
+                if (line[2] == 'Z')
+                {
+                    if (line[0] == 'A')
+                        x = 'Y';
+                    if (line[0] == 'B')
+                        x = 'Z';
+                    if (line[0] == 'C')
+                        x = 'X';
+                }
+                int p = GetPoints(line[0], x);
+                sum += p;
+
+            }
+
+
+            Console.WriteLine(sum);
+        }
+
+        private static int GetPoints(char l1, char l2)
+        {
+            int p = 0;
+            if (l2 == 'X')
+            {
+                p += 1;
+                if (l1 == 'A')
+                    p += 3;
+                if (l1 == 'B')
+                    p += 0;
+                if (l1 == 'C')
+                    p += 6;
+            }
+            if (l2 == 'Y')
+            {
+                p += 2;
+                if (l1 == 'A')
+                    p += 6;
+                if (l1 == 'B')
+                    p += 3;
+                if (l1 == 'C')
+                    p += 0;
+            }
+            if (l2 == 'Z')
+            {
+                p += 3;
+                if (l1 == 'A')
+                    p += 0;
+                if (l1 == 'B')
+                    p += 6;
+                if (l1 == 'C')
+                    p += 3;
+            }
+
+            return p;
         }
     }
 }
