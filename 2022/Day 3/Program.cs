7795aeb0a4dd66d8181d2de5ae2356defa93c47f
Henrik Kurelid <henrik@kurelid.se>
2022-12-03 06:13:38 +0100

    Day 3

--- 2022/Day 3/Program.cs
+++ 2022/Day 3/Program.cs
@@ -7,7 +7,43 @@ namespace AdventOfCode
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                string had = "";
+                string s1 = line.Substring(0, line.Length / 2);
+                string s2 = line.Substring(line.Length / 2);
+                foreach (char c in s1)
+                {
+                    if (!had.Contains(c) && s2.Contains(c))
+                    {
+                        sum += GetPrio(c);
+                        had = had + c;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+            sum = 0;
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                string had = "";
+                foreach (char c in lines[i])
+                {
+                    if (!had.Contains(c) && lines[i + 1].Contains(c) && lines[i + 2].Contains(c))
+                    {
+                        sum += GetPrio(c);
+                        had= had + c;
+                    }
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+        private static int GetPrio(char c)
+        {
+            if (c <= 'Z')
+                return c - 'A' + 27;
+            return c - 'a' + 1;
         }
     }
 }
