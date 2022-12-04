cdd972acded0faa2f7bd46a93036a7542515d5a2
Henrik Kurelid <henrik@kurelid.se>
2022-12-04 06:11:49 +0100

    Day 4

--- 2022/Day 4/Program.cs
+++ 2022/Day 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Principal;
 
 namespace AdventOfCode
 {
@@ -7,7 +8,28 @@ namespace AdventOfCode
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int c = 0;
+            int c2 = 0;
+            foreach (string line in lines)
+            {
+                string[] pair = line.Split(',');
+                string[] p1 = pair[0].Split('-');
+                string[] p2 = pair[1].Split('-');
+                int i1 = int.Parse(p1[0]);
+                int i2 = int.Parse(p1[1]);
+                int i3 = int.Parse(p2[0]);
+                int i4 = int.Parse(p2[1]);
+                if (i1 >= i3 && i2 <= i4)
+                    c++;
+                else if (i3 >= i1 && i4 <= i2)
+                    c++;
+                if (i1 <= i4 && i2 >= i3)
+                    c2++;
+                else if (i3 <= i2 && i4 >= i1)
+                    c2++;
+            }
+            Console.WriteLine(c);
+            Console.WriteLine(c2);
         }
     }
 }
