540504bf96b648d3032c527491bfcf89418c1fe5
Henrik Kurelid <henrik@kurelid.se>
2022-12-06 06:11:32 +0100

    Day 6

--- 2022/Day 6/Program.cs
+++ 2022/Day 6/Program.cs
@@ -7,7 +7,30 @@ namespace AdventOfCode
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+
+            Calculate(lines, 4);
+            Calculate(lines, 14);
+        }
+
+        private static void Calculate(string[] lines, int count)
+        {
+            for (int i = count - 1; i < lines[0].Length; i++)
+            {
+                bool found = true;
+                for (int j = 0; j < count - 1; j++)
+                {
+                    for (int k = j + 1; k < count; k++)
+                    {
+                        if (lines[0][i - j] == lines[0][i - k])
+                            found = false;
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
         }
     }
 }
