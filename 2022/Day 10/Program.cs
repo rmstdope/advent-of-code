ce39ed9b34880272b5f593c10dcbfce2c83ef2e0
Henrik Kurelid <henrik@kurelid.se>
2022-12-10 06:33:22 +0100

    Day 10

--- 2022/Day 10/Program.cs
+++ 2022/Day 10/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace AdventOfCode
 {
@@ -7,7 +9,57 @@ namespace AdventOfCode
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int str = 1;
+            int sum = 0;
+            int i = 0;
+            int s = 0;
+            int cycle = 0;
+            string[] crt = new string[6];
+            while (cycle <= 239)
+            {
+                int y = cycle / 40;
+                int x = cycle % 40;
+                if (x == (str - 1) || x == (str + 0) || x == (str + 1))
+                {
+                    if (y < 6)
+                        crt[y] += "#";
+                }
+                else
+                {
+                    if (y < 6)
+                        crt[y] += ".";
+                }
+
+                cycle++;
+                if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
+                {
+                    sum += cycle * str;
+                }
+                string[] cmd = lines[i].Split(' ');
+                if (cmd[0] == "addx")
+                {
+                    if (s == 1)
+                    {
+                        str += int.Parse(cmd[1]);
+                        i++;
+                        s = 0;
+                    }
+                    else
+                    {
+                        s = 1;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            Console.WriteLine(sum);
+            for (int j = 0; j < 6; j++)
+            {
+                Console.WriteLine(crt[j]);
+            }
         }
     }
 }
