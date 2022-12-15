5896a92fc93f16e40357b70b663d16b0fc17e284
Henrik Kurelid <henrik@kurelid.se>
2022-12-15 07:17:57 +0100

    Day 15

--- 2022/Day 14/Program.cs
+++ 2022/Day 14/Program.cs
@@ -2,12 +2,178 @@
 
 namespace AdventOfCode
 {
+
     class Program
     {
+        static public int width;
+        static public int height;
+        static public char[] map;
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            int minX = 500;
+            int maxX = 500;
+            int maxY = 0;
+            foreach (string line in lines)
+            {
+                string[] c = line.Split(" -> ");
+                foreach (string s in c)
+                {
+                    string[] values = s.Split(",");
+                    minX = Math.Min(minX, int.Parse(values[0]));
+                    maxX = Math.Max(maxX, int.Parse(values[0]));
+                    maxY = Math.Max(maxY, int.Parse(values[1]));
+                }
+            }
+            minX -= 1;
+            maxX += 1;
+            width = (maxX - minX + 1);
+            height = (maxY + 1);
+            map = new char[width * height];
+            for (int i = 0; i < map.Length; i++)
+            {
+                map[i] = '.';
+            }
+            foreach (string line in lines)
+            {
+                string[] c = line.Split(" -> ");
+                for (int i = 0; i < c.Length - 1; i++)
+                {
+                    string[] values1 = c[i].Split(",");
+                    string[] values2 = c[i + 1].Split(",");
+                    if (values1[0] != values2[0])
+                    {
+                        int x1 = int.Parse(values2[0]) - minX;
+                        int x2 = int.Parse(values1[0]) - minX;
+                        if (x1 > x2)
+                        {
+                            int t = x1;
+                            x1 = x2;
+                            x2 = t;
+                        }
+                        int y = int.Parse(values1[1]);
+                        for (int j = x1; j <= x2; j++)
+                        {
+                            map[y * width + j] = '#';
+                        }
+                    }
+                    else
+                    {
+                        int x = int.Parse(values1[0]) - minX;
+                        int y1 = int.Parse(values1[1]);
+                        int y2 = int.Parse(values2[1]);
+                        if (y1 > y2)
+                        {
+                            int t = y1;
+                            y1 = y2;
+                            y2 = t;
+                        }
+                        for (int j = y1; j <= y2; j++)
+                        {
+                            map[j * width + x] = '#';
+                        }
+                    }
+                }
+            }
+
+            int numsands = Simulate(minX);
+            Console.WriteLine(numsands);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[y * width + x] == 'o')
+                        map[y * width + x] = '.';
+                }
+            }
+            int newHeight = height + 2;
+            int newWidth = width + height * 2;
+            char[] newMap = new char[newHeight * newWidth];
+            for (int y = 0; y < newHeight; y++)
+            {
+                for (int x = 0; x < newWidth; x++)
+                {
+                    if (y == newHeight - 1)
+                        newMap[y * newWidth + x] = '#';
+                    else
+                        newMap[y * newWidth + x] = '.';
+                }
+            }
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    newMap[y * newWidth + x + height] = map[y * width + x];
+                }
+            }
+            minX -= height;
+            width = newWidth;
+            height = newHeight;
+            map = newMap;
+
+            numsands = Simulate(minX);
+            //DrawMap();
+            Console.WriteLine(numsands);
+        }
+
+        private static int Simulate(int minX)
+        {
+            bool endless = true;
+            int numsands = 0;
+            while (endless)
+            {
+                int sandX = 500 - minX;
+                int sandY = 0;
+                bool rest = false;
+                while (!rest)
+                {
+                    if (sandX == 0 || sandX == width - 1 || sandY == height - 1)
+                    {
+                        endless = false;
+                        rest = true;
+                    }
+                    else
+                    {
+                        if (map[(sandY + 1) * width + sandX] == '.')
+                        {
+                            sandY += 1;
+                        }
+                        else if (map[(sandY + 1) * width + sandX - 1] == '.')
+                        {
+                            sandY += 1;
+                            sandX -= 1;
+                        }
+                        else if (map[(sandY + 1) * width + sandX + 1] == '.')
+                        {
+                            sandY += 1;
+                            sandX += 1;
+                        }
+                        else
+                        {
+                            map[sandY * width + sandX] = 'o';
+                            rest = true;
+                            numsands++;
+                            if (sandY == 0)
+                                endless = false;
+                        }
+                    }
+                }
+            }
+
+            return numsands;
+        }
+
+        private static void DrawMap()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(map[y * width + x]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- 2022/Day 15/Program.cs
+++ 2022/Day 15/Program.cs
@@ -1,13 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AdventOfCode
 {
     class Program
     {
+        static public List<int[]> pairs = new();
         static void Main()
         {
             string[] lines = System.IO.File.ReadAllLines("../../../input.txt");
-            Console.WriteLine("Hello World!");
+            for (int i = 0;i < lines.Length;i++)
+            {
+                string[] div = lines[i].Split(' ');
+                int[] pair = new int[5];
+                pair[0] = int.Parse(div[2].Substring(2, div[2].Length - 3));
+                pair[1] = int.Parse(div[3].Substring(2, div[3].Length - 3));
+                pair[2] = int.Parse(div[8].Substring(2, div[8].Length - 3));
+                pair[3] = int.Parse(div[9].Substring(2));
+                pair[4] = Math.Abs(pair[2] - pair[0]) + Math.Abs(pair[3] - pair[1]);
+                pairs.Add(pair);
+            }
+            int num = 0;
+            int yRow = 2000000;
+            //int yRow = 10;
+            for (int x = -5000000; x < 6000000; x++)
+            {
+                bool couldBe = true;
+                foreach (int[] pair in pairs)
+                {
+                    int d1 = Math.Abs(x - pair[0]) + Math.Abs(yRow - pair[1]);
+                    int d2 = Math.Abs(pair[2] - pair[0]) + Math.Abs(pair[3] - pair[1]);
+                    bool same = pair[2] == x && pair[3] == yRow;
+                    if (d1 <= d2 && !same)
+                        couldBe = false;
+                }
+                if (!couldBe)
+                    num++;
+            }
+            Console.WriteLine(num);
+
+            int maxTest = 4000000;
+            //int maxTest = 20;
+            List<int[]> intersects = new List<int[]>();
+            int[] start = new int[2];
+            for (int y = 0; y < maxTest; y++)
+            {
+                intersects.Clear();
+                start[0] = 0;
+                start[1] = maxTest;
+                intersects.Add(start);
+                foreach (int[] pair in pairs)
+                {
+                    int dY = Math.Abs(y - pair[1]);
+                    int xRest = pair[4] - dY;
+                    if (xRest > 0)
+                    {
+                        int[] ranges = new int[4];
+                        ranges[0] = 0;
+                        ranges[1] = pair[0] - xRest - 1;
+                        ranges[2] = pair[0] + xRest + 1;
+                        ranges[3] = maxTest;
+                        Intersect(intersects, ranges);
+                    }
+                }
+                if (intersects.Count > 0)
+                {
+                    Int64 g = (Int64)intersects[0][0] * 4000000 + (Int64)y;
+                    Console.WriteLine(g);
+                }
+            }
+        }
+
+        private static void Intersect(List<int[]> intersects, int[] ranges)
+        {
+            List<int[]> newIntersects = new();
+            foreach (int[] inter in intersects)
+            {
+                int[] newIntersect = new int[2];
+                if (ranges[1] >= ranges[0])
+                {
+                    int[] x = new int[2];
+                    x[0] = Math.Max(inter[0], ranges[0]);
+                    x[1] = Math.Min(inter[1], ranges[1]);
+                    if (x[0] <= x[1])
+                        newIntersects.Add(x);
+                }
+                if (ranges[2] <= ranges[3])
+                {
+                    int[] x = new int[2];
+                    x[0] = Math.Max(inter[0], ranges[2]);
+                    x[1] = Math.Min(inter[1], ranges[3]);
+                    if (x[0] <= x[1])
+                        newIntersects.Add(x);
+                }
+            }
+            intersects.Clear();
+            intersects.AddRange(newIntersects);
         }
     }
 }
