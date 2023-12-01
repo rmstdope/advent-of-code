c91e476861b03f737ee0cef2ad717fd7eb2018d4
Henrik Kurelid <henrik@kurelid.se>
2023-12-01 07:34:28 +0100

    Both parts in same run

--- 2023/Day 1/day.py
+++ 2023/Day 1/day.py
@@ -6,36 +6,32 @@ import re
 f = open('prod.txt')
 strs = f.read().splitlines()
 
-part1 = 0
-numbers = ['<>', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-for s in strs:
-    num = ''
-    for i in range(len(s)):
-        if s[i] <= '9' and s[i] >= '0':
-            num += s[i]
-            break
-        found = False
-        for i2,n in enumerate(numbers):
-            if s[i:i +  len(n)] == n:
-                num += str(i2)
-                print(n)
-                found = True
-        if found:
-            break
-    for i in reversed(range(len(s))):
-        if s[i] <= '9' and s[i] >= '0':
-            num += s[i]
-            break
-        found = False
-        for i2,n in enumerate(numbers):
-            if s[i:i + len(n)] == n:
-                num += str(i2)
-                print(n)
-                found = True
-        if found:
-            break
-    print(num)
-    part1 += int(num)
-part2 = 0
+def get_number(s, i, text):
+    numbers = ['<>', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    if s[i] <= '9' and s[i] >= '0':
+        return s[i]
+    for i2,n in enumerate(numbers):
+        if text and s[i:i +  len(n)] == n:
+            return str(i2)
+    return ''
+
+def get_cal_values(strs, text):
+    val = 0
+    for s in strs:
+        num = ''
+        for i in range(len(s)):
+            num += get_number(s, i, text)
+            if len(num) == 1:
+                break
+        for i in reversed(range(len(s))):
+            num += get_number(s, i, text)
+            if len(num) == 2:
+                break
+        #print(num)
+        val += int(num)
+    return val
+
+part1 = get_cal_values(strs, False)
+part2 = get_cal_values(strs, True)
 
 print(f'{part1=}, {part2=}')
